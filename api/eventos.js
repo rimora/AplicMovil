34460df179703f458ea7161e0836512e7badf052
Ricardo Morales <rimora7@hotmail.com>
2013-05-21 16:04:49 -0500

    clientes
    clientes

--- api/eventos.js
+++ api/eventos.js
@@ -1,15 +1,30 @@
 // EVENTOS
 $(document).ready(function() {
 	//document.addEventListener("deviceready",function(){
-		var db = window.openDatabase("Sardel", "1.0", "SardelDB", 1000000);
+	/*	var db = window.openDatabase("Sardel", "1.0", "SardelDB", 1000000);
     $('#clientes').click(function() {
 		db.transaction(consulta,errorconsulta,listo);
 		
 	
-	});
+	});*/
+$("li").click(function() {
+                 var oID = $(this).attr("id");
+				  //alert (oID);
+				  $.mobile.changePage($("#datoscli"))
+				  
+				  
+				  
+               });
+
+$('#datoscli').live('pageshow',function(event, ui){
+
+//alert('This page was just hidden: '+ ui.prevPage);
+$("#nomcli").text("Nombre:   hola2")
+$("#salcli").text("Saldo:    $1000.00")
+$("#dircli").text("Dirección:dirección del cliente")
+});
 
 
-		
-  
+
 });
 
