6601bf9f532b5d0927d1ff9c2b2218f61997088e
Ricardo Morales <rimora7@hotmail.com>
2013-05-30 12:27:44 -0500

    34
    34

--- api/consultas.js
+++ api/consultas.js
@@ -53,7 +53,7 @@ function insertar(){
         tx.executeSql('INSERT INTO CLIENTES (nombre,clave,dia,direccion,telefono,tipo,diasc,lcredito,saldo) VALUES ("Farmacia DOS", "1030","Martes","Dirección del cliente  DOS","2281545130","C","30","10000.00","5000.00")'); 
 		 tx.executeSql('INSERT INTO erpadmin_alcxc_pen_cob (cod_zon,cod_tip_dc,num_doc,cod_clt,saldo,monto,fec_doc_ft,fec_ven) VALUES ("S04", "1","00041534","1020","$437.55","$437.55","08/05/2013","08/05/2013")');        
 		 tx.executeSql('INSERT INTO erpadmin_alcxc_pen_cob (cod_zon,cod_tip_dc,num_doc,cod_clt,saldo,monto,fec_doc_ft,fec_ven) VALUES ("S04", "1","00041535","1020","$888.55","$1000.55","15/05/2013","15/05/2013")');        
-		 tx.executeSql('INSERT INTO erpadmin_alcxc_pen_cob (cod_zon,cod_tip_dc,num_doc,cod_clt,saldo,monto,fec_doc_ft,fec_ven) VALUES ("S04", "1","00041536","1020","$5000.00","$5000.00","08/06/2013","08/06/2013")');        
+		 tx.executeSql('INSERT INTO erpadmin_alcxc_pen_cob (cod_zon,cod_tip_dc,num_doc,cod_clt,saldo,monto,fec_doc_ft,fec_ven) VALUES ("S04", "1","00041536","1030","$5000.00","$5000.00","08/06/2013","08/06/2013")');        
 
 
 		}
@@ -96,47 +96,57 @@ function mostrarcliente(clavecli){
 //  $('#datoscli').live('pageshow',function(event, ui){
    	   window.localStorage.clear();
 	   saveidcliente(clavecli);
-		alert('entra mostrar cliente');
+		//alert('entra mostrar cliente');
 		var db = window.openDatabase("Database", "1.0", "SARDEL", 200000);
 		db.transaction(consulta, errorconsulta);
 	
 	function consulta(tx) {
-		tx.executeSql('SELECT * FROM CLIENTES WHERE clave="'+clavecli+'"',[],exito,errorconsulta);    	
+		tx.executeSql('SELECT * FROM CLIENTES a left outer join erpadmin_alcxc_pen_cob b on b.cod_clt=a.clave WHERE clave="'+clavecli+'"',[],exito,errorconsulta);
+		tx.executeSql('SELECT * FROM erpadmin_alcxc_pen_cob WHERE cod_clt="'+clavecli+'"',[],poblarfac,errorconsulta);    	
 		//alert('entro a la consulta de datos de un cliente');
 		}
 	
-	function exito(tx,results){         
-	   var row = results.rows.item(0);            
-	   $('#nomcli').text("Nombre: "+row['nombre']);
-  	   $('#clacli').text("Clave: "+row['clave']);
-	   $('#direccion').text("Dirección: "+row['direccion']);
-  	   $('#telefono').text("Telefono: "+row['telefono']);
-	   $('#tipo').text("Tipo: "+row['tipo']);
-  	   $('#diascredito').text("Dias de Crédito: "+row['diasc']);
-	   $('#limitecredito').text("Límite de Crédito: "+row['lcredito']);
-	   $('#saldo').text("Saldo: "+row['saldo']);
-	   
-	   $("#gridfaccli").empty();
-	    var html = "";
-					 html += "<div class=ui-block-a><strong>Tipo</strong> El texto que se ecriba aquí se amoldará a la mitad de pagina.</div>";
-					 html += "<div class=ui-block-b><strong>Documento</strong> El texto que se ecriba aquí se amoldará a la otra mitad de pagina.</div>";
-                     html += "<div class=ui-block-c><strong>Vence</strong> El texto que se ecriba aquí se amoldará a la mitad de pagina.</div>";
-					 html += "<div class=ui-block-d><strong>Saldo</strong> El texto que se ecriba aquí se amoldará a la otra mitad de pagina.</div>";
-                     html += "<div class=ui-block-e><strong>Monto</strong> El texto que se ecriba aquí se amoldará a la otra mitad de pagina.</div>";
+		function exito(tx,results){         
+	   		var row = results.rows.item(0);            
+	   		$('#nomcli').text("Nombre: "+row['nombre']);
+	   	    $('#clacli').text("Clave: "+row['clave']);
+		    $('#direccion').text("Dirección: "+row['direccion']);
+  	   		$('#telefono').text("Telefono: "+row['telefono']);
+	   		$('#tipo').text("Tipo: "+row['tipo']);
+  	   		$('#diascredito').text("Dias de Crédito: "+row['diasc']);
+	   		$('#limitecredito').text("Límite de Crédito: "+row['lcredito']);
+	   		$('#saldo').text("Saldo: "+row['saldo']);
+		}
+		function exito(tx,results){ 
+		      $("#gridfaccli").empty();			  
+			  var html = "";
+			  var tipo="";
+			  $.each(results.rows,function(index){
+				  var row = results.rows.item(index); 				     
+				     if (row['tipo']=="1"){
+						 tipo="FACTURA"
+					 }
+					 else  {
+						 tipo="OTRO" 
+					 }
+					 
+					 html += "<div class=ui-block-a><strong>" +tipo+"</strong> El texto que se ecriba aquí se amoldará a la mitad de pagina.</div>";
+					 html += "<div class=ui-block-b><strong>"+row['num_doc']+"</strong> El texto que se ecriba aquí se amoldará a la otra mitad de pagina.</div>";
+                     html += "<div class=ui-block-c><strong>"+row['fec_ven']+"</strong> El texto que se ecriba aquí se amoldará a la mitad de pagina.</div>";
+					 html += "<div class=ui-block-d><strong>"+row['saldo']+"</strong> El texto que se ecriba aquí se amoldará a la otra mitad de pagina.</div>";
+                     html += "<div class=ui-block-e><strong>"+row['monto']+"</strong> El texto que se ecriba aquí se amoldará a la otra mitad de pagina.</div>";
 
-                  	 html += "<div class=ui-block-a><strong>Tipo</strong> El texto que se ecriba aquí se amoldará a la mitad de pagina.</div>";
-					 html += "<div class=ui-block-b><strong>Documento</strong> El texto que se ecriba aquí se amoldará a la otra mitad de pagina.</div>";
-                     html += "<div class=ui-block-c><strong>Vence</strong> El texto que se ecriba aquí se amoldará a la mitad de pagina.</div>";
-					 html += "<div class=ui-block-d><strong>Saldo</strong> El texto que se ecriba aquí se amoldará a la otra mitad de pagina.</div>";
-                     html += "<div class=ui-block-e><strong>Monto</strong> El texto que se ecriba aquí se amoldará a la otra mitad de pagina.</div>";
-		$("#gridfaccli").append(html);  	   
- 		}
+                  	 
+			  });
+					$("#gridfaccli").append(html);  
+	   }
+ 		
 	function errorconsulta(err) {
-    	alert("Error SQL al poblar cliente: "+err);
+    	alert("Error SQL al poblar cliente: "+err.code+err.message);
 	}
 //  });	
 
-}
+  }//funcion consulta(x)
 function guardacliente(nombre,empresa,rfc,direccion,colonia,estado,municipio,telefono){
 	consultadb().transaction(nuevocli,function(err){
     	  alert("Error al insertar cliente: "+err.code+err.message);
