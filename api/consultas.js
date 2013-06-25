5a5974bbf037763a63050d1560c9ef9723b88b0e
Ricardo Morales <rimora7@hotmail.com>
2013-06-25 13:24:53 -0500

    86
    86

--- api/consultas.js
+++ api/consultas.js
@@ -152,7 +152,32 @@ function preparadetalletemp(articulo,cantidad){
 	   //para obtener el importe de descuento:
 	   // dividir entre 100 el precio, multiplicar el resultado por el descuento y se obtiene el importe de descuento
 	   //restar el importe de descuento al precio
-	   var exis=existencia(articulo);
+	   var exis=function(articulo){
+		   					alert('articulo de funcion existencia '+articulo);	
+							function listo(tx,results){ 	 
+						      alert('entra a listo de consulexis');         
+						      if (results.rows.length>0){			  
+				                 var row = results.rows.item(index);    
+								 existenciab=row['existencia'];			
+					             alert('existencia de consulta '+existenciab);
+							  }		
+							  else{
+								  existenciab=0;
+							  alert('no hay existencias funcion existencia'); 
+							  }
+							  return existenciab;
+						 	}
+							function consulexis(tx){   	
+						        alert('entra a consulexis');    
+				    			var sql='SELECT existencia FROM ARTICULO_EXISTENCIA WHERE articulo="'+articulo+'" AND bodega="K01"';			
+								tx.executeSql(sql,[],listo,function(err){
+				    	 		 alert("Error consultar existencia : "+err.code+err.message);
+         						});    									
+							}
+							consultadb().transaction(consulexis, function(err){
+				    	 		 alert("Error select tabla ARTICULO_EXISTENCIA: "+err.code+err.message);
+         					});	
+			   }
 	   var diferencia=exis-cantidad;
 	   alert('existencia '+exis);
 	   alert('cantidad '+exis);
